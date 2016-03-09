47ff0df9a63a03e05ddc61e6c49ebb6be10dcc5e
Lndqw12 <COMP222012016@mail.ru>
2016-03-09 15:27:11 +0300

    Sofrver1
    Sofrver1

---
+++ file1.php
@@ -0,0 +1,6 @@
+﻿<?php
+$a = array(5+6);
+$b = array(2+10);
+print_r($a);
+print_r($b);
+?>
---
+++ file2.php
@@ -0,0 +1,5 @@
+<?php
+$sum=$a+$b = array;
+echo "¬ведите A = <input type=\"text\" name=\"$i\"><br>"; #спамим поле ввода
+echo "¬ведите B = <input type=\"text\" name=\"$i\"><br>"; #спамим поле ввода
+?>
